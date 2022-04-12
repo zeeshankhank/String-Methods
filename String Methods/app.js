fb72967ef3d858df6553a23af762257fe7ae4318
Zeshan khan <98458839+zeeshankhank@users.noreply.github.com>
2022-04-12 06:49:27 +0500

    Add files via upload

---
+++ String Methods/app.js
@@ -0,0 +1,73 @@
+// Question no #1 //
+
+// var Firstname = prompt("Enter First Name");
+// var lastname =  prompt("Enter last Name");
+// alert(Firstname+lastname)
+
+// Question no #2 //
+
+// var string = " samsung galaxy s6 edge plus";
+// var len = string.length;
+// document.write("length of string"+string.length)
+ 
+// Question no #3 //
+
+// var user ="pakistan";
+// var indexNumber = user.indexOf("n")
+// document.write("index of 'n' :"  + indexNumber)
+
+// Question no #4 //
+
+// var user ="hello world";
+// var indexNumber = user.lastIndexOf("l")
+// document.write(" last index of 'l' :"  + indexNumber)
+
+// Questin on #5 //
+
+// var user ="pakistani";
+// var indexNumber = user.charAt("3")
+// document.write(" character at index  '3' :"  + indexNumber)
+
+// Question no #6 //
+
+// Question no #7 //
+
+// Question no #8 //
+
+// Question no #9 //
+
+// Question no #10 //
+
+// var text = "peanuts!";
+// var result = text.toUpperCase();
+// document.write(result)
+
+// Question no #11 //
+
+// var text = "java script!";
+// var result = text.toUpperCase(0);
+// document.write(result)
+
+// Question no #12 //
+
+// var num = 35.36;
+// var without = num.toString().replace(".", ""); 
+// document.write(without)
+
+// Question no #13 //
+
+// Question no #14 //
+
+// var user = +prompt ["cake", "apple pie", "cookie", "chips", "patties"]
+
+// // "wellcome to abc bakery.what do you want to order sir/ma'am?
+
+// Question no #15 //
+
+// Question no #16 //
+
+// Question no #17 //
+
+// var user ="pakistani";
+// var indexNumber = user.charAt("7")
+// document.write(" character at index  :"  + indexNumber)
